d4ce453056155a2db3092d6fb49e9944620f61fd
Moamen Eltouny <raggigroup@gmail.com>
2026-01-11 17:46:28 +0200

    Update PM2 config generation to use bun.template.js and adjust script execution

--- configs/bun.template.js
+++ configs/bun.template.js
@@ -1,17 +1,14 @@
 module.exports = {
-    apps: [
-        {
-            name: "{{APP_NAME}}",
-            script: "bun",
-            args: "next start -p {{APP_PORT}}",
-            cwd: "{{APP_PATH}}",
-            interpreter: "none",
-            autorestart: true,
-            restart_delay: 2000,
-            kill_timeout: 5000,
-            env: {
-                NODE_ENV: "production",
-            }
-        }
-    ]
+  apps: [
+    {
+      name: "{{APP_NAME}}",
+      script: "{{APP_TYPE}}",
+      args: "run start -- -p {{APP_PORT}}",
+      cwd: "{{APP_PATH}}",
+      env: {
+        NODE_ENV: "production",
+      },
+      watch: false,
+    }
+  ]
 };
